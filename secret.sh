ecf237e0684a4bf668421d05a9556b827a196bbe
kasojura <rameshkumar.kasoju2@gmail.com>
2020-09-11 19:51:07 +0530

    modified

--- secret.sh
+++ secret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Please enter your age:"
+echo "Please enter your age: "
 read age
-[ $age -lt 20 ] && { echo sorry; exit 1;} || echo Welcome
+[ $age -le 20 ] && { echo sorry; exit 1;} || echo Welcome
 	echo "Secrets shouldn't be revealed"
